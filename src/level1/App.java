c069faff650288404bb3f80ede2513c3b1530519
sylimi2r2 <sylimi2r2@naver.com>
2025-05-03 23:23:15 +0900

    level1

---
+++ src/level1/App.java
@@ -0,0 +1,69 @@
+package level1;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // exit 전까지 무한루프
+        while (true) {
+            // 양의 정수 input
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int leftOperand = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int rightOperand = sc.nextInt();
+
+            // 연산 기호 input
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            // 연산자에 따른 result 계산
+            int result = 0;
+            boolean isDouble = false;
+            switch (operator) {
+                case '+':
+                    result = leftOperand + rightOperand;
+                    break;
+                case '-':
+                    result = leftOperand - rightOperand;
+                    break;
+                case '*':
+                    result = leftOperand * rightOperand;
+                    break;
+                case '/':
+                    // zero divisor 방지
+                    if (rightOperand == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
+
+                    // 나눗셈 결과가 소수인 경우 판별
+                    if (leftOperand % rightOperand != 0) {
+                        isDouble = true;
+                        break;
+                    }
+
+                    result = leftOperand / rightOperand;
+                    break;
+                default:
+                    System.out.println("잘못된 연산자를 입력하셨습니다.");
+                    continue;
+            }
+
+            // 결과 출력 결과가 소수인 경우에 double로 연산
+            if (isDouble)
+                System.out.println("결과: " + (double)leftOperand / rightOperand);
+            else
+                System.out.println("결과: " + result);
+
+            // 종료할지 확인
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String command = sc.next();
+            if (command.equals("exit")) {
+                System.out.println("종료 중...");
+                break;
+            }
+        }
+    }
+}
